822c2b75850c1123b4bdf0e33371e13fe8ec1f55
Damián Guzmán Martinez <damianguzmanmartinez7@gmail.com>
2025-11-05 09:46:15 -0600

    Update ej9_fizzbuzz.c

--- ej9_fizzbuzz.c
+++ ej9_fizzbuzz.c
@@ -1,12 +1,66 @@
 #include <stdio.h>
+#include <ctype.h>
+
+void limpiarBuffer()
+{
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF)
+        ;
+}
 
 int main()
 {
-    int N;
+    int N, numero = 0, caracter, valido = 0;
 
-    printf("\tIngrese el número hasta donde se evaluará la serie FizzBuzz: ");
-    scanf("%d", &N);
-    printf("\t");
+    while (!valido)
+    {
+        printf("\tIngrese el numero hasta donde se evaluara la serie FizzBuzz: ");
+        caracter = getchar();
+
+        if (caracter == '-')
+        {
+            printf("\tLos numeros negativos no estan permitidos.\n");
+            limpiarBuffer();
+            continue;
+        }
+
+        if (isdigit(caracter))
+        {
+            numero = caracter - '0';
+            while ((caracter = getchar()) != '\n' && caracter != EOF)
+            {
+                if (isdigit(caracter))
+                {
+                    numero = numero * 10 + (caracter - '0'); // obtener lo números
+                }
+                else
+                {
+                    numero = -1; // invalido
+                    limpiarBuffer();
+                    break;
+                }
+            }
+
+            if (numero >= 0)
+            {
+                valido = 1;
+            }
+            else
+            {
+                printf("\tIngrese solo números.\n");
+            }
+        }
+        else
+        {
+            printf("\tIngrese solo números.\n");
+            limpiarBuffer();
+        }
+    }
+
+    N = numero;
+    printf("\n\tEntrada: %i", N);
+
+    printf("\n\t↳Salida: ");
     for (int i = 1; i < N + 1; i++)
     {
         if (i % 3 == 0 && i % 5 == 0)
